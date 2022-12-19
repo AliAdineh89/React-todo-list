c06a24b6d16ba2c9d8ccc0d1f33348635f4cccf4
AdinehAli <ahoora.ali@gmx.at>
2022-12-19 15:39:14 +0100

    todolist

---
+++ src/Screens/Todo.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { useState } from "react";
+import TodoList from "../Todo/TodoList";
+import { v4 as uuid } from "uuid";
+
+const Todo = ({ searchQuery }) => {
+  const [todos, setTodos] = useState([
+    { title: "Test Todo", id: uuid(), done: false },
+  ]);
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
+
+  const handleAddTodo = (event) => {
+    if (event.key === "Enter") {
+      setTodos([
+        ...todos,
+        { title: event.target.value, id: uuid(), done: false },
+      ]);
+      event.target.value = "";
+    }
+  };
+
+  const handleToggleTodo = (id) => {
+    setTodos(
+      todos.map((t) => {
+        if (t.id === id) {
+          return { ...t, done: !t.done };
+        }
+        return t;
+      })
+    );
+  };
+
+  const activeTodos = todos.filter(({ done }) => !done);
+  const filteredTodos = todos.filter((t) =>
+    t.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  const doneTodos = todos.filter(({ done }) => done);
+  return (
+    <>
+      <div className="TodoInputContainer">
+        <input
+          onKeyDown={handleAddTodo}
+          placeholder="Hier Todo hinzufügen"
+          className="InputItem"
+        />
+      </div>
+      {searchQuery ? (
+        <TodoList
+          title="Suchergebnisse"
+          list={filteredTodos}
+          onDeleteTodo={handleDeleteTodo}
+          onToggleTodo={handleToggleTodo}
+        />
+      ) : (
+        <>
+          <TodoList
+            title="Zu erledigen"
+            list={activeTodos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
+          <TodoList
+            title="Erledigt"
+            list={doneTodos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Todo;
